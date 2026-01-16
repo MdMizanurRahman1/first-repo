140bbfd57cb092508bc927d2b111d7c24a87ec5c
mizan <mizan389rahman@gmail.com>
2026-01-16 10:46:22 +0200

    committed

--- hello.py
+++ hello.py
@@ -1,2 +1,7 @@
-print("hello world")
-print("hi, everyone")
+# Python module-1 assignment-1.
+#1
+print("Hello, Mizanur Rahman! Welocome to Python classes at Metropolia UAS!")
+
+print("Hello, Viivi Virta!")
+
+#2
